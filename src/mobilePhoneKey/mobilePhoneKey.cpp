9f08a6ec37dddd1d0d91b46b0dbde8af80e59430
Caique <asapcaique7@gmail.com>
2024-07-26 22:00:02 -0300

    solução de mobilePhoneKey funcionando, mas pra aprimoração pra todos testes

--- src/mobilePhoneKey/mobilePhoneKey.cpp
+++ src/mobilePhoneKey/mobilePhoneKey.cpp
@@ -8,6 +8,18 @@ using namespace std;
 vector<string> buttons = {"1.,?!", "2abc", "3def", "4ghi", "5jkl", "6mno", "7pqrs", "8tuv", "9wxyz", "*'-+=", "0", "#"};
 
 std::pair<int, int> contains_char(char target) {
+  if (target == '*') {
+        return {9, 0}; // Retorna 9 para '*' (posição 1)
+    } else if (target == '\'') {
+        return {9, 1}; // Retorna 9 para '\'' (posição 2)
+    } else if (target == '-') {
+        return {9, 2}; // Retorna 9 para '-' (posição 3)
+    } else if (target == '+') {
+        return {9, 3}; // Retorna 9 para '+' (posição 4)
+    } else if (target == '=') {
+        return {9, 4}; // Retorna 9 para '=' (posição 5)
+    }
+  
     for (size_t i = 0; i < buttons.size(); ++i) {
         const string& digit = buttons[i];
         size_t pos = digit.find(target);
@@ -44,8 +56,14 @@ string send_message(const string& message) {
                     pressedButtons.back() += to_string(buttonPressed.first);
                 }
             }
-        } else if (c == '-' || c == '+' || c == '=') {
-            pressedButtons.push_back("*");
+        }  else if (c == '-' || c == '+' || c == '=') {
+          auto buttonPressed = contains_char(c);
+          if (buttonPressed.first != -1) {
+               char symbolFirstChar = buttons[buttonPressed.first][0];
+              for (int i = 0; i < buttonPressed.second; ++i){
+            pressedButtons.back() += symbolFirstChar; 
+              }
+            }
         }
          else {
             // Adiciona a sequência para caracteres especiais
@@ -58,7 +76,6 @@ string send_message(const string& message) {
             }
         }
         
-        // Adiciona um espaço se a próxima tecla é a mesma
         if (pressedButtons.size() > 1 && isalpha(c) &&
             pressedButtons.back()[0] == pressedButtons[pressedButtons.size() - 2][0]) {
             pressedButtons.insert(pressedButtons.end() - 1, " "); // Insere um espaço antes do último botão
@@ -72,6 +89,8 @@ string send_message(const string& message) {
     }
     return result;
 }
+
+
 
 int main() {
 string message = "Heyy";
