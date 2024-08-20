4c6f37a0f1fdd819011ed9f0283290bb51cd9155
Caique <asapcaique7@gmail.com>
2024-08-20 09:17:19 -0300

    retornando numeros em forma crescente

--- src/sortedNumbers/sortedNumbers.cpp
+++ src/sortedNumbers/sortedNumbers.cpp
@@ -1,10 +1,15 @@
+#include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include <numeric>
 
 std::vector<int> solution(std::vector<int> nums) {
-    if (!nums.empty()) {
-        std::vector<int> sortedItems;
-        for (char c : nums){
-            sortedItems.push_back(c);
-            }
+    std::vector<int> sortedNums = nums;
+    if (!sortedNums.empty()) {
+        std::sort(sortedNums.begin(), sortedNums.end());
+        return sortedNums;
     }
+    return {};
 }
